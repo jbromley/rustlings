f4bc0eaf6a477bad641505fde4d43b769198079d
J. Bromley <jay@appliedinvention.com>
2024-04-12 12:29:44 -0700

    Completed exercises for macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,16 +5,19 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
+        };
+        ( $x:expr ) => {
+            println!("This macro has one argument: {}", $x);
         };
     }
 }
 
 fn main() {
     my_macro!();
+    my_macro!(17);
 }
